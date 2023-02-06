ea6719bd0f9197f8b439fd11f7fc0c7f9ee9222f
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2023-02-06 13:09:47 -0700

    rebuilt website

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-
-
 // ES6 Class
 class TypeWriter {
-  constructor(txtElement, words, wait = 8000) {
+  constructor(txtElement, words, wait = 3000) {
     this.txtElement = txtElement;
     this.words = words;
-    this.txt = "";
+    this.txt = '';
     this.wordIndex = 0;
-    this.wait = parseInt(wait, 12);
+    this.wait = parseInt(wait, 10);
     this.type();
     this.isDeleting = false;
   }
@@ -19,7 +17,7 @@ class TypeWriter {
     const fullTxt = this.words[current];
 
     // Check if deleting
-    if (this.isDeleting) {
+    if(this.isDeleting) {
       // Remove char
       this.txt = fullTxt.substring(0, this.txt.length - 1);
     } else {
@@ -31,28 +29,42 @@ class TypeWriter {
     this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
     // Initial Type Speed
-    let typeSpeed = 200;
+    let typeSpeed = 300;
 
-    if (this.isDeleting) {
+    if(this.isDeleting) {
       typeSpeed /= 2;
     }
 
     // If word is complete
-    if (!this.isDeleting && this.txt === fullTxt) {
+    if(!this.isDeleting && this.txt === fullTxt) {
       // Make pause at end
       typeSpeed = this.wait;
       // Set delete to true
       this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === "") {
+    } else if(this.isDeleting && this.txt === '') {
       this.isDeleting = false;
       // Move to next word
       this.wordIndex++;
       // Pause before start typing
-      typeSpeed = 200;
+      typeSpeed = 500;
     }
 
     setTimeout(() => this.type(), typeSpeed);
   }
 }
+
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
+
 
 
